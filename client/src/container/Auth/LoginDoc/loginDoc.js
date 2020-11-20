5306d47bf11f3bc32bb9bc77b76d99db2a637992
harshshaw <harshshaw5@gmail.com>
2020-11-21 00:36:41 +0530

    test

--- client/src/container/Auth/LoginDoc/loginDoc.js
+++ client/src/container/Auth/LoginDoc/loginDoc.js
@@ -33,7 +33,7 @@ class Login extends Component {
         this.setState({ errMessage: err.message });
       }); */
 
-    await Axios.post("/api/auth", {
+    await Axios.post("/loginDoc", {
       email: this.state.email,
       password: this.state.password,
     })
@@ -62,86 +62,86 @@ class Login extends Component {
     return (
       <div className="logInDoc">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Sign-Up
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Sign-Up
               </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/signDoc">As a Doctor</a>
-                <a className="dropdown-item" href="/signPt">As a Patient</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/#">Contact Us <span className="sr-only"></span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div className="row text-center">
-        <div className="col-lg-8">
-          <img alt="..."src={img} className="logImg"/>
-        </div>
-        <div className="col-lg-4">
-          <div className="shadow-lg p-3  rounded">
-            <h2>Log In</h2>
-            <div className="p-3">
-              <form>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <h6 className="head"><i className="fas fa-envelope fa-lg"></i></h6>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      type="email"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      id="email"
-                      name="email"
-                      placeholder="Email"
-                      onChange={this.handleChange}
-                      value={this.state.email}
-                    />
-                  </div>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="/signDoc">As a Doctor</a>
+                  <a className="dropdown-item" href="/signPt">As a Patient</a>
                 </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <h6 className="head"><i className="fas fa-key fa-lg"></i></h6>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Contact Us <span className="sr-only"></span></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="row text-center">
+          <div className="col-lg-8">
+            <img alt="..." src={img} className="logImg" />
+          </div>
+          <div className="col-lg-4">
+            <div className="shadow-lg p-3  rounded">
+              <h2>Log In</h2>
+              <div className="p-3">
+                <form>
+                  <div className="row">
+                    <div className="col-sm-2">
+                      <h6 className="head"><i className="fas fa-envelope fa-lg"></i></h6>
+                    </div>
+                    <div className="col-sm-10">
+                      <input
+                        type="email"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                        value={this.state.email}
+                      />
+                    </div>
                   </div>
-                  <div className="col-sm-10">
-                    <input
-                      name="password"
-                      type="password"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                      id="password"
-                      placeholder="Password"
-                      value={this.state.password}
-                    />
-                  </div>  
-                </div>
-                <div>
-                  <button onClick={this.login} className="btn btn-primary logInDocBtn">
-                    Log In
+                  <div className="row">
+                    <div className="col-sm-2">
+                      <h6 className="head"><i className="fas fa-key fa-lg"></i></h6>
+                    </div>
+                    <div className="col-sm-10">
+                      <input
+                        name="password"
+                        type="password"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        onChange={this.handleChange}
+                        id="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <button onClick={this.login} className="btn btn-primary logInDocBtn">
+                      Log In
                   </button>
-                </div> 
+                  </div>
                 New User? Sign Up  as <Link to="/signDoc">Doctor</Link> or <Link to="/signPt">Patient</Link>
-              </form>
-              <p className="para">{this.state.errMessage}</p>
+                </form>
+                <p className="para">{this.state.errMessage}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
--- client/src/container/Auth/SignPt/signupPt.js
+++ client/src/container/Auth/SignPt/signupPt.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
 
-import img  from "./undraw_unlock_24mb.svg"
+import img from "./undraw_unlock_24mb.svg"
 import "./signupPt.css";
 import Axios from "axios";
 import { localsName } from "ejs";
@@ -25,17 +25,14 @@ class Signup extends Component {
   }
   signup = async (e) => {
     e.preventDefault();
-    await Axios.post("/api/users", {
+    await Axios.post("http://localhost:5000/register", {
       name: this.state.name,
       email: this.state.email,
       password: this.state.password,
-      address: this.state.address,
-      location: this.state.location,
-      phone: this.state.phone,
+      city: this.state.location,
     })
       .then((res) =>
-        this.setState({ valid: res.data.token, userId: res.data.userId })
-      )
+        console.log('done'))
       .catch((err) => console.log(err));
   };
   handleChange(e) {
@@ -53,147 +50,147 @@ class Signup extends Component {
     return (
       <div className="signupPt">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Log In
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Log In
               </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="/loginDoc">As a Doctor</a>
-                <a className="dropdown-item" href="/loginPt">As a Patient</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/#">Contact Us <span className="sr-only"></span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-          <div className="rounded row">
-            <div className="col-lg-8">
-              <img alt="..."src={img} />
-            </div>
-            <div className="col-lg-4 shadow-lg text-center rounded">
-            <h4> Sign-Up for Patient </h4> 
-              <form>
-               <div className="row">
-                  <div className="col-sm-2">
-                    <label><i className="fas fa-signature fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      type="text"
-                      name="name"
-                      className="form-control"
-                      placeholder="Enter your Name"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                      value={this.state.name}
-                    ></input>
-                  </div>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="/loginDoc">As a Doctor</a>
+                  <a className="dropdown-item" href="/loginPt">As a Patient</a>
                 </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label htmlFor="exampleInputEmail1"><i className="fas fa-envelope fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Enter your email"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label><i className="fas fa-key fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      value={this.state.password}
-                      name="password"
-                      type="password"
-                      placeholder="Enter Password"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label><i className="fas fa-phone-alt fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      value={this.state.phone}
-                      name="phone"
-                      type="Number"
-                      placeholder="Enter your Phone No"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                   <label><i className="fas fa-city fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      value={this.state.address}
-                      type="text"
-                      name="address"
-                      placeholder="Enter your City"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-sm-2">
-                    <label><i className="fas fa-map-marked-alt fa-lg"></i></label>
-                  </div>
-                  <div className="col-sm-10">
-                    <input
-                      value={this.state.location}
-                      type="text"
-                      name="location"
-                      placeholder="Enter your State"
-                      className="form-control"
-                      aria-describedby="emailHelp"
-                      onChange={this.handleChange}
-                    ></input>
-                  </div>
-                </div>
-                <div className="text-center">
-                  <button
-                    type="submit"
-                    onClick={this.signup}
-                    className="btn btn-primary signInPtBtn"
-                    style={{ marginTop: "20px" }}
-                  >
-                    Sign-up
-                  </button>
-                </div> 
-              </form>
-            </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Contact Us <span className="sr-only"></span></a>
+              </li>
+            </ul>
           </div>
+        </nav>
+        <div className="rounded row">
+          <div className="col-lg-8">
+            <img alt="..." src={img} />
+          </div>
+          <div className="col-lg-4 shadow-lg text-center rounded">
+            <h4> Sign-Up for Patient </h4>
+            <form>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label><i className="fas fa-signature fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Enter your Name"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label htmlFor="exampleInputEmail1"><i className="fas fa-envelope fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label><i className="fas fa-key fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    value={this.state.password}
+                    name="password"
+                    type="password"
+                    placeholder="Enter Password"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label><i className="fas fa-phone-alt fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    value={this.state.phone}
+                    name="phone"
+                    type="Number"
+                    placeholder="Enter your Phone No"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label><i className="fas fa-city fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    value={this.state.address}
+                    type="text"
+                    name="address"
+                    placeholder="Enter your City"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-2">
+                  <label><i className="fas fa-map-marked-alt fa-lg"></i></label>
+                </div>
+                <div className="col-sm-10">
+                  <input
+                    value={this.state.location}
+                    type="text"
+                    name="location"
+                    placeholder="Enter your State"
+                    className="form-control"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleChange}
+                  ></input>
+                </div>
+              </div>
+              <div className="text-center">
+                <button
+                  type="submit"
+                  onClick={this.signup}
+                  className="btn btn-primary signInPtBtn"
+                  style={{ marginTop: "20px" }}
+                >
+                  Sign-up
+                  </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
