f233b23f7726b7010df4478df0beca895190ed85
harshshaw <harshshaw5@gmail.com>
2020-11-21 18:51:26 +0530

    fixed error

--- client/src/container/DoctorView/DoctorView.js
+++ client/src/container/DoctorView/DoctorView.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
-const DoctorView = ({ id }) => {
+const DoctorView = () => {
 
     const [Doctors, updateDoctor] = useState([]);
 
